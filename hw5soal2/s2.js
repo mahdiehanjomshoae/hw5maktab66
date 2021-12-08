773a00107149bfd873685d68c69140968f95b84a
mahdieh <mahdieh.anjomshoae98@gmail.com>
2021-12-08 17:09:38 +0330

    complete 2

--- hw5soal2/s2.js
+++ hw5soal2/s2.js
@@ -1,23 +1,38 @@
-function maxNumber(){
-    const x = Number.MAX_SAFE_INTEGER
-    for(let index in str){
-        if(str==' ' || str[index]===x){
-           console.log("big error")
-         }
-     }
-}
-maxNumber() // null 
-maxNumber(2 ** 53 - 1) // 9007199254740991n 
-maxNumber(2 ** 53) // "Big Error" 
-maxNumber("") // "Big Error
-
-
-
-
-// maxNumber(1n, 10n, 5n) // 10n 
-// maxNumber("10", 5n, 1) // 10n 
-// maxNumber("10" / 3) // "Big Error" 
-//  maxNumber("10" / 2) // 5n 
-// maxNumber(10 - 12, 10 - 10, 10 - 11) // 0n 
-// maxNumber(0n) // 0n 
- 
+ function maxNumber(){
+    let max = arguments[0]
+    let y
+    try{
+        if(arguments.length === 0) throw `"null"`
+        for(let i = 0 ; i<arguments.length ; i++){ 
+            if(typeof arguments[i] !== "bigint"){
+                if(typeof arguments[i] !== "string"){
+                    if( !Number.isInteger(arguments[i])){
+                        throw `"Big Error"`
+                    }
+                }
+            } 
+            if(arguments[i] === "" ||  (BigInt(arguments[i])  >= 2 ** 53)){
+                throw `"Big Error"`
+            }else{ 
+                y = BigInt(arguments[i])
+                if(y >= max){
+                    max = y
+                }
+            }
+        }
+        return max
+    }
+    catch(error){
+        return error
+    }
+ }
+ console.log(maxNumber(1n, 10n, 5n))// 10n 
+ console.log(maxNumber("10", 5n, 1)) // 10n 
+ console.log(maxNumber(2 ** 53)) // "Big Error" 
+ console.log(maxNumber("")) // "Big Error
+ console.log(maxNumber()) // null 
+ console.log(maxNumber(2 ** 53 - 1) )// 9007199254740991n 
+ console.log(maxNumber("10" / 3)) // "Big Error" 
+ console.log(maxNumber("10" / 2) )// 5n 
+ console.log(maxNumber(10 - 12, 10 - 10, 10 - 11)) // 0n 
+ console.log(maxNumber(0n)) // 0n 
